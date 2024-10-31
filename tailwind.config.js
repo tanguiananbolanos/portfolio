9c6cd02b81b5243ae84931faf58ab53074b09d04
Tan <mjguiananbolanos@gmail.com>
2024-10-31 20:41:52 +0800

    2nd commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,53 @@ module.exports = {
           "0%": { transform: "translate(100px)", opacity: "0%" },
           "100%": { transform: "translate(0%)" },
         },
+        opacity: {
+          "0%": {
+            opacity: "0%",
+          },
+          "100%": {
+            opacity: "100%",
+          },
+        },
+        borderAnimate: {
+          "0%": {
+            borderLeft: "solid white 1px",
+            borderBottom: "none",
+            borderRight: "none",
+            borderTop: "none",
+          },
+          "25%": {
+            borderLeft: "solid white 1px",
+            borderBottom: "none",
+            borderRight: "none",
+            borderTop: "none",
+          },
+          "50%": {
+            borderLeft: "none",
+            borderBottom: "solid white 1px",
+            borderRight: "none",
+            borderTop: "none",
+          },
+          "75%": {
+            borderLeft: "none",
+            borderBottom: "none",
+            borderRight: "solid white 1px",
+            borderTop: "none",
+          },
+          "100%": {
+            borderLeft: "none",
+            borderBottom: "none",
+            borderRight: "none",
+            borderTop: "solid white 1px",
+          },
+        },
       },
       animation: {
         spinY: "spin 5s linear infinite",
         enter: "enter 2s ease-in",
         reverse: "enterReverse 2s ease-in",
+        opacity: "opacity 2s ease-in",
+        borderAnimate: "borderAnimate 5s ease-in-out infinite",
       },
     },
     screens: {
