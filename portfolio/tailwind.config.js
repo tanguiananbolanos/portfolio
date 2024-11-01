f684e02a62ca8296c526e19d3534e06f75bc489c
Tan <mjguiananbolanos@gmail.com>
2024-11-01 23:24:02 +0800

    added some animation touch

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -2,7 +2,23 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        flydiag: {
+          "0%": {
+            transform: "translateY(70px)",
+          },
+          "100%": {
+            transform: "translateY(-1000px) translateX(500px)",
+          },
+        },
+      },
+      animation: {
+        flyZ: "flydiag 5s linear infinite",
+        flyX: "flydiag 10s linear infinite",
+        flyY: "flydiag 3s linear infinite",
+      },
+    },
   },
   plugins: [],
 };
